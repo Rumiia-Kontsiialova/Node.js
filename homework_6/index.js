7ae66cfb6ac0f4641a5369f3342ba01ac5ba99dd
Rumiia Kontsiialova <rumiia.kontsiyalova@gmail.com>
2025-08-22 22:11:48 +0200

    third part of 6 homework added

--- homework_6/index.js
+++ homework_6/index.js
@@ -8,6 +8,24 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (_, res) => {
+    try {
+        res.send('Hello, World!');
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
+
+app.post('/', (req, res) => {
+    const data = req.body;
+    if (!data || !data.name) {
+    return res.status(400).json({ message: 'The name field is required' });
+  }
+    res.status(201).json({ message: `Data received: ${data.name}` });
+});
 
 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
