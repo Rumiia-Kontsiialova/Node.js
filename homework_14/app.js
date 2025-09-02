d4867f2c255b64fd6425f3bc0a0ec916d949af00
Rumiia Kontsiialova <rumiia.kontsiyalova@gmail.com>
2025-09-02 20:52:39 +0200

    completed and updated homework 14

--- homework_14/app.js
+++ homework_14/app.js
@@ -1,28 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-// import userRouter from './routes/userRoutes.js';
-// import postRouter from './routes/postRoutes.js';
-// import updateRouter from './routes/userRoutes.js';
+import connectToDatabase from './config/db.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3003;
 
 const app = express();
 
 app.use(express.json());
 
-// app.use('/users', userRouter);
-
-// app.use('/posts', postRouter);
-
+app.use('/categories', categoryRoutes);
+app.use('/products', productRoutes);
 
 app.listen(PORT, async () => {
-    try {
-        await connectDB();
-        console.log(`Server is running on http://localhost:${PORT}`);
-    } catch (error) {
-        console.error('Оибка при запуске сервера');
-    }
+  try {
+    await connectToDatabase();
+    console.log(`Server is running on port: http://localhost:${PORT}`);
+  } catch (error) {
+    console.error('Error starting server:', error);
+  }
 });
---
+++ homework_14/routes/productRoutes.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import Product from '../models/Product.js';
+
+const router = express.Router();
+
+router.post('/create', async (req, res) => {
+  const { name, price, category } = req.body;
+  try {
+    const newProduct = new Product({ name, price, category });
+    await newProduct.save();
+    res
+      .status(201)
+      .send({ message: 'Product created successfully', product: newProduct });
+  } catch (error) {
+    res.status(500).send({ message: 'Error creating product', error });
+  }
+});
+
+router.get('/all', async (req, res) => {
+  try {
+    const products = await Product.find({}).populate('category');
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(500).send({ message: 'Error fetching products', error });
+  }
+});
+
+
+export default router;
